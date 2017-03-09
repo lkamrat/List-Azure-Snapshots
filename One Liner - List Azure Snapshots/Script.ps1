cf50dfbd7e68c9b0a1b275a1785a036462f1b4ec
lkamrat <kamrat9@gmail.com>
2017-03-09 13:15:45 +0200

    G2G

--- One Liner - List Azure Snapshots/Script.ps1
+++ One Liner - List Azure Snapshots/Script.ps1
@@ -1,3 +1,4 @@
 ﻿#
-# Script.ps1
-#
+# This is "one-liner" PowerShell script to list all of Azure managed disks snapshots
+
+Get-AzureRmSnapshot | Format-Table -Property Name,@{Name="Resource Group"; Expression={$_.ResourceGroupName}; Alignment="center"; },@{Name="Size in GB"; Expression={$_.DiskSizeGB}; Alignment="center"; } -AutoSize -Wrap
